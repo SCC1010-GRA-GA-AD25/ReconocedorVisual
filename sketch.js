c3c537dedc67b2534daa2ef111a3a57663605c53
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-12-03 20:20:26 -0700

    feat(recognizer): create initial recognizer with base model integration

--- sketch.js
+++ sketch.js
@@ -1,14 +1,61 @@
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = './model/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
 function preload() {
-  // put preload code here
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 }
 
 function setup() {
-  // put setup code here
-  createCanvas(800,800)
+  createCanvas(320, 260);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  classifyVideo();
 }
 
 function draw() {
-  // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+  background(0);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+  flippedVideo.remove();
+
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
 }
